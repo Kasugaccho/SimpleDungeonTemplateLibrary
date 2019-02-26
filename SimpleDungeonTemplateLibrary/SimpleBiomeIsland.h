c9ec8852fcf9112b5a326d558820bbd4c0d680c6
Kasugaccho <30593725+Kasugaccho@users.noreply.github.com>
2019-02-27 02:19:03 +0900

    add sbi

---
+++ SimpleDungeonTemplateLibrary/SimpleBiomeIsland.h
@@ -0,0 +1,52 @@
+﻿#ifndef INCLUDED_DUNGEON_TEMPLATE_LIBRARY_C89_BIOME_ISLAND
+#define INCLUDED_DUNGEON_TEMPLATE_LIBRARY_C89_BIOME_ISLAND
+//:::::----------::::::::::----------::::://
+//     Dungeon Template Library     //
+//          Made by Gaccho.          //
+// This code is licensed under CC0.  //
+//       wanotaitei@gmail.com       //
+//:::::----------::::::::::----------::::://
+
+#include <stdlib.h>
+
+void createSimpleBiomeIsland(int** matrix_, int x_, int y_, int max_value_, int type_) {
+
+	int i, row, col;
+
+	for (row = 1; row < y_ - 1; ++row) {
+		for (col = 1; col < x_ - 1; ++col) {
+			if (((int)(((double)rand() / ((double)RAND_MAX + 1)) * 2))) {
+				matrix_[row][col] = ((int)(((double)rand() / ((double)RAND_MAX + 1)) * type_));
+			}
+		}
+	}
+
+	for (i = 0; i < max_value_; ++i) {
+		for (row = 1; row < y_ - 1; ++row) {
+			for (col = 1; col < x_ - 1; ++col) {
+				if (matrix_[row][col - 1] && matrix_[row][col + 1] && matrix_[row - 1][col] && matrix_[row + 1][col]) {
+					matrix_[row][col] = matrix_[row][col + 1];
+				}
+				else {
+					switch (((int)(((double)rand() / ((double)RAND_MAX + 1)) * 4)))
+					{
+					case 0:
+						matrix_[row][col] = matrix_[row][col - 1];
+						break;
+					case 1:
+						matrix_[row][col] = matrix_[row][col + 1];
+						break;
+					case 2:
+						matrix_[row][col] = matrix_[row - 1][col];
+						break;
+					case 3:
+						matrix_[row][col] = matrix_[row + 1][col];
+						break;
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif /*Included Dungeon Template Library*/
---
+++ SimpleDungeonTemplateLibrary/Source.c
@@ -0,0 +1,52 @@
+﻿#include "SimpleBiomeIsland.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void) {
+
+	int **dungeon, *dungeon_base_matrix;
+	int y = 128, x = 128;
+	int i, row, col;
+
+	srand((unsigned int)time(NULL));
+
+	dungeon = (int **)malloc(y * sizeof(int *));
+	if (dungeon == NULL) return -1;
+	dungeon_base_matrix = (int *)calloc(y * x, sizeof(int));
+	if (dungeon_base_matrix == NULL) return -1;
+	for (i = 0; i < y; ++i) {
+		dungeon[i] = dungeon_base_matrix + i * x;
+	}
+
+	createSimpleBiomeIsland(dungeon, x, y, 50, 4);
+
+	for (row = 1; row < y; ++row) {
+		for (col = 1; col < x; ++col) {
+			switch (dungeon[row][col])
+			{
+			case 0:
+				printf("//");
+				break;
+			case 1:
+				printf("##");
+				break;
+			case 2:
+				printf("**");
+				break;
+			case 3:
+				printf("++");
+				break;
+			}
+		}
+		printf("\n");
+	}
+
+
+
+	free(dungeon_base_matrix);
+	free(dungeon);
+
+	return 0;
+}
